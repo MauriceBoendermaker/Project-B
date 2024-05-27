5ae61e47e96333ae41b250755107d023fe258b52
Alper Sonmez <aziaatyt@gmail.com>
2024-05-27 15:17:26 +0200

    Added PrintReservationUser method

--- MegaBios/MegaBios/ReservationHistory.cs
+++ MegaBios/MegaBios/ReservationHistory.cs
@@ -11,7 +11,7 @@ namespace MegaBios
 
         [JsonPropertyName("movie_title")]
         public string MovieTitle { get; set; }
-        
+
         [JsonPropertyName("reserved_seats")]
         public List<Seat> ReservedSeats { get; set; }
 
@@ -24,63 +24,77 @@ namespace MegaBios
         // public double TotalPrice {get; set;}
 
 
-        public ReservationHistory(string reservationNumber, string movieTitle, List<Seat> reservedSeats, string reservationRoom, DateTime reservationDate) {
+        public ReservationHistory(string reservationNumber, string movieTitle, List<Seat> reservedSeats, string reservationRoom, DateTime reservationDate)
+        {
             ReservationNumber = reservationNumber;
             MovieTitle = movieTitle;
             ReservedSeats = reservedSeats;
             ReservationRoom = reservationRoom;
             ReservationDate = reservationDate;
             // TotalPrice = totalPrice;
-        } 
+        }
 
         // TODO: Add property that saves the reserved seat(s) so it can be iterated over to unoccupy seats in case of ticket cancellation
 
-        public static void AddReservation(TestAccount account, ReservationHistory reservationHistory) {
+        public static void AddReservation(TestAccount account, ReservationHistory reservationHistory)
+        {
             List<TestAccount> accounts = JsonFunctions.LoadCustomers("../../../customers.json");
-            for (int i = 0; i < accounts.Count; i++) {
-                if (accounts[i] == account) {
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                if (accounts[i] == account)
+                {
                     accounts[i].History.Add(reservationHistory);
                     JsonFunctions.WriteToJson("../../../customers.json", accounts);
                     break;
-                } 
+                }
             }
 
-        }   
+        }
 
-        public static bool IsReservationIDTaken(string reservationID) {
+        public static bool IsReservationIDTaken(string reservationID)
+        {
             List<TestAccount> accounts = JsonFunctions.LoadCustomers("../../../customers.json");
-            foreach(TestAccount account in accounts) {
-                foreach(ReservationHistory reservation in account.History) {
-                    if (reservation.ReservationNumber == reservationID) {
+            foreach (TestAccount account in accounts)
+            {
+                foreach (ReservationHistory reservation in account.History)
+                {
+                    if (reservation.ReservationNumber == reservationID)
+                    {
                         return true;
                     }
                 }
             }
             return false;
-        } 
+        }
 
-        public static string generateReservasionNumber() {
+        public static string generateReservasionNumber()
+        {
             Random rand = new Random();
-            while(true) {
+            while (true)
+            {
                 int reservationNumber = rand.Next(0, 10000);
-                if (!IsReservationIDTaken(reservationNumber.ToString())) {
+                if (!IsReservationIDTaken(reservationNumber.ToString()))
+                {
                     return reservationNumber.ToString();
                 }
             }
-            
+
         }
 
-        public static bool ConfirmPayment(ReservationHistory reservation) {
-            List<string> menuOptions = new() {"Ja", "Nee"};
+        public static bool ConfirmPayment(ReservationHistory reservation)
+        {
+            List<string> menuOptions = new() { "Ja", "Nee" };
             int selectedOption = MenuFunctions.Menu(menuOptions, PrintReservation(reservation));
             return selectedOption == 0 ? true : false;
         }
 
-        public static StringBuilder PrintReservation(ReservationHistory reservation) {
+        public static StringBuilder PrintReservation(ReservationHistory reservation)
+        {
             StringBuilder reservationPrint = new StringBuilder();
             string stoelenString = "";
             double totalPrice = 0;
-            for (int i = 0; i < reservation.ReservedSeats.Count; i++) {
+            for (int i = 0; i < reservation.ReservedSeats.Count; i++)
+            {
                 Seat seat = reservation.ReservedSeats[i];
                 stoelenString += $"{seat.SeatNumber}: {seat.Price:F2} Euro\n";
                 totalPrice += seat.Price;
@@ -93,18 +107,40 @@ namespace MegaBios
             return reservationPrint;
         }
 
-        public static List<Seat> ApplyDiscount(List<Seat> selectedSeats, TestAccount account) {
+        public static string PrintReservationUser(ReservationHistory reservation)
+        {
+            string stoelenString = "";
+            double totalPrice = 0;
+            Seat seat; // Declare seat outside the loop
+            for (int i = 0; i < reservation.ReservedSeats.Count; i++)
+            {
+                seat = reservation.ReservedSeats[i]; // Assign value inside the loop
+                stoelenString += $"{seat.SeatNumber}: {seat.Price:F2} Euro\n";
+                totalPrice += seat.Price;
+            }
+            return $"--------RESERVERING DATA-----------\n" +
+                $"Reserveringsnummer: {reservation.ReservationNumber}\n" +
+                $"Film: {reservation.MovieTitle}\n" +
+                $"Gereserveerde stoelen:\n{stoelenString}" + // Use stoelenString here
+                $"Reserveringszaal: {reservation.ReservationRoom}\n" +
+                $"Reserveringsdatum: {reservation.ReservationDate}\n";
+        }
+
+        public static List<Seat> ApplyDiscount(List<Seat> selectedSeats, TestAccount account)
+        {
             double discount = 0;
-            if (account.IsStudent) {
+            if (account.IsStudent)
+            {
                 discount = 0.15;
             }
-            for (int i = 0; i < selectedSeats.Count; i++) {
+            for (int i = 0; i < selectedSeats.Count; i++)
+            {
                 selectedSeats[i].Price *= 1 - discount;
             }
             return selectedSeats;
         }
     }
 
-    
+
 
 }
