2f17a11e1da595793465b26fe77f3ee301a7922f
Alper Sonmez <aziaatyt@gmail.com>
2024-06-09 21:28:47 +0200

    Created Test for Cinemaroom Creation as Admin

---
+++ MegaBios/MegaBiosTest/CinemaRoomGeneratorTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Threading;
+using MegaBios;
+
+namespace MegaBiosTest.Services
+{
+    [TestClass]
+    public class CinemaRoomGeneratorTests
+    {
+        private string filePath = "../../../Room1.json";
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // Stel de omgevingsvariabele in voor de testomgeving
+            Environment.SetEnvironmentVariable("IS_TEST_ENVIRONMENT", "true");
+
+            // Zorg ervoor dat het testbestand niet bestaat voor de test
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // Verwijder de omgevingsvariabele na de test
+            Environment.SetEnvironmentVariable("IS_TEST_ENVIRONMENT", null);
+
+            // Verwijder het testbestand na de test
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        private void SuppressConsoleOutput(Action action)
+        {
+            var originalOut = Console.Out;
+            var originalError = Console.Error;
+
+            try
+            {
+                using (var writer = new StringWriter())
+                {
+                    Console.SetOut(writer);
+                    Console.SetError(writer);
+                    action();
+                }
+            }
+            finally
+            {
+                Console.SetOut(originalOut);
+                Console.SetError(originalError);
+            }
+        }
+
+        [TestMethod]
+        public void GenerateShowingData_ShouldCreateNewRoom()
+        {
+            // Arrange
+            var generator = new CinemaRoomGenerator();
+            var input = new StringReader("1\n10\n10\n2024-01-01 10:00:00\n");
+            Console.SetIn(input);
+
+            // Act
+            SuppressConsoleOutput(() =>
+            {
+                generator.GenerateShowingData();
+            });
+
+            // Assert
+            Thread.Sleep(500); // Kleine vertraging om bestandssysteem te laten verwerken
+            var fileExists = File.Exists(filePath);
+            Assert.IsTrue(fileExists, "Room1.json is niet aangemaakt.");
+        }
+    }
+}
