be9435736588c4df2d97a4db6143465370953242
nhojW <161321640+nhojW@users.noreply.github.com>
2024-06-09 14:53:03 +0200

    Added unittest for LoveSeat price check

---
+++ MegaBios/MegaBiosTest/LoveSeatPriceTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MegaBios;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace MegaBiosTest.Services
+{
+    [TestClass]
+    public class SeatPricingTests
+    {
+        [TestMethod]
+        public void LoveSeatPricing_LoveSeatsHaveDifferentPrice()
+        {
+            // Arrange
+            var seatingPlan = new List<List<Seat>>
+            {
+                new List<Seat>
+                {
+                    new Seat { SeatType = "normal", Price = 10.00 },
+                    new Seat { SeatType = "love seat", Price = 20.00 },
+                    new Seat { SeatType = "love seat", Price = 20.00 }
+                }
+            };
+
+            // Act
+            // No action required, as pricing is predefined in the seating plan
+
+            // Assert
+            // Ensure love seats have a different price than normal seats
+            Assert.AreEqual(10.00, seatingPlan[0][0].Price); // Normal seat
+            Assert.AreEqual(20.00, seatingPlan[0][1].Price); // Love seat 1
+            Assert.AreEqual(20.00, seatingPlan[0][2].Price); // Love seat 2
+        }
+    }
+}
