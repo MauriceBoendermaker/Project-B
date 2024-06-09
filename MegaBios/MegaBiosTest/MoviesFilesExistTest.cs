9eb3da6cfe758268c67b601a28ad0366d64a306d
Alper Sonmez <aziaatyt@gmail.com>
2024-06-09 23:13:42 +0200

    Created Test to find a Movies file for the Admin
    bestaat er een movies.json of movies.csv?

---
+++ MegaBios/MegaBiosTest/MoviesFilesExistTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace MegaBiosTest.Services
+{
+    [TestClass]
+    public class MoviesFilesExistTest
+    {
+        private string jsonFilePath = "../../../../MegaBios/Movies.json";
+        private string csvFilePath = "../../../../MegaBios/Movies.csv";
+
+        [TestMethod]
+        public void CheckIfMoviesJsonOrCsvExists()
+        {
+            // Print the absolute paths for debugging
+            string absoluteJsonPath = Path.GetFullPath(jsonFilePath);
+            string absoluteCsvPath = Path.GetFullPath(csvFilePath);
+
+            Console.WriteLine($"Checking JSON Path: {absoluteJsonPath}");
+            Console.WriteLine($"Checking CSV Path: {absoluteCsvPath}");
+
+            // Assert
+            bool jsonExists = File.Exists(jsonFilePath);
+            bool csvExists = File.Exists(csvFilePath);
+
+            Assert.IsTrue(jsonExists || csvExists, "Movies.json of Movies.csv bestaat niet in de map MegaBios.");
+        }
+    }
+}
