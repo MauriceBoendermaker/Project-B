bfd0feb9cab0e77b04d954d5c7d7cf32768406f5
nhojW <161321640+nhojW@users.noreply.github.com>
2024-06-09 15:46:01 +0200

    Love seat selection Unit Test

--- MegaBios/MegaBiosTest/LoveSeatPriceTests.cs
+++ MegaBios/MegaBiosTest/LoveSeatPriceTests.cs
@@ -31,5 +31,59 @@ namespace MegaBiosTest.Services
             Assert.AreEqual(20.00, seatingPlan[0][1].Price); // Love seat 1
             Assert.AreEqual(20.00, seatingPlan[0][2].Price); // Love seat 2
         }
+        [TestMethod]
+        public void CheckForLoveSeats()
+        {
+            // Arrange
+            var roomShowings = new List<RoomShowing>(); // Provide valid room showings
+            string roomNumber = "Room 1"; // Provide a valid room number
+            DateTime showTime = DateTime.Now; // Provide a valid show time
+            Account reservingAccount = new Account(
+                "Daan",
+                "", // Tussenvoegsel (middle name) seems to be empty in your example
+                "Bakker",
+                "1975-04-20",
+                new Dictionary<string, string>
+                {
+                    { "straat", "Voorstraat" },
+                    { "huisnummer", "45" },
+                    { "woonplaats", "Rotterdam" },
+                    { "postcode", "3011 HN" }
+                },
+                "daan.bakker@example.com",
+                "G3heim!",
+                "+31 10 98765432",
+                "Creditcard",
+                false,
+                new List<Reservation>(),
+                new List<Reservation>()
+            );
+
+            // Mock or provide a valid account instance
+            List<List<Seat>> seating = new List<List<Seat>>
+            {
+                new List<Seat>
+                {
+                    new Seat { SeatType = "normal", SeatTaken = false },
+                    new Seat { SeatType = "love seat", SeatTaken = false },
+                    new Seat { SeatType = "normal", SeatTaken = false }
+                },
+                new List<Seat>
+                {
+                    new Seat { SeatType = "normal", SeatTaken = false },
+                    new Seat { SeatType = "normal", SeatTaken = false },
+                    new Seat { SeatType = "love seat", SeatTaken = false }
+                }
+            };
+
+            SeatSelect seatSelect = new SeatSelect(roomShowings, roomNumber, showTime, reservingAccount);
+            seatSelect.Seats = seating;
+
+            // Act
+            bool hasLoveSeats = seatSelect.HasLoveSeats();
+
+            // Assert
+            Assert.IsTrue(hasLoveSeats);
+        }
     }
 }
