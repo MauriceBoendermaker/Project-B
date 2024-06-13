0590b654ce3ace4f4a77a6be945a6485ec7d898a
MayorMayonnaise <xanderbos04@hotmail.com>
2024-06-13 14:30:42 +0200

    Small qol for editing account data

--- MegaBios/MegaBios/UpdateAccount.cs
+++ MegaBios/MegaBios/UpdateAccount.cs
@@ -19,82 +19,73 @@ namespace MegaBios
                 Console.WriteLine("Voor een of andere reden was uw account niet gevonden");
                 return;
             }
+            List<string> menuOptions = new() {"E-Mail", "Wachtwoord", "Student status"};
+            // Console.WriteLine("1. Email2. Wachtwoord\n3. Student status");
+            int selectedChoice = MenuFunctions.Menu(menuOptions, null, true);
 
-            Console.WriteLine("Wat wilt u updaten?\n1. Email\n2. Wachtwoord\n3. Student status");
-            Console.WriteLine("Voer alleen het nummer van de optie in die u wilt updaten!");
-            Console.WriteLine("Voer je keuze in: ");
 
-            bool loopBreak = false;
 
-            while (true)
+            
+            switch (selectedChoice)
             {
-                switch (Console.ReadLine())
-                {
-                    case "1":
-                        Console.WriteLine("Voer de nieuwe email in");
+                case -1:
+                    return;
+                case 0:
+                    Console.WriteLine("Voer de nieuwe email in");
 
-                        string newEmail = Console.ReadLine()!;
+                    string newEmail = Console.ReadLine()!;
 
-                        Program.jsonData[index].Email = newEmail;
-                        JsonFunctions.WriteToJson("../../../customers.json", Program.jsonData);
-
-                        loopBreak = true;
-                        break;
-                    case "2":
-                        while (true)
-                        {
-                            Console.WriteLine("Voer het nieuwe wachtwoord in");
-                            string newPassword = Console.ReadLine()!;
-                            Console.WriteLine("Bevestig het wachtwoord");
-
-                            if (newPassword == Console.ReadLine())
-                            {
-                                Program.jsonData[index].Wachtwoord = newPassword;
-                                JsonFunctions.WriteToJson("../../../customers.json", Program.jsonData);
-
-                                loopBreak = true;
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Wachtwoorden komen niet overeen!");
-                            }
-                        }
-                        break;
-                    case "3":
-                        Console.WriteLine("Bent u student? (ja/nee)");
-
-                        while (true)
-                        {
-                            string studentInput = Console.ReadLine()!;
-
-                            if (studentInput == "ja")
-                            {
-                                Program.jsonData[index].IsStudent = true;
-                                break;
-                            }
-                            else if (studentInput == "nee")
-                            {
-                                Program.jsonData[index].IsStudent = false;
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Ongeldige invoer!");
-                            }
-                        }
-
-                        JsonFunctions.WriteToJson("../../../customers.json", Program.jsonData);
-
-                        loopBreak = true;
-                        break;
-                }
-
-                if (loopBreak)
-                {
+                    Program.jsonData[index].Email = newEmail;
+                    JsonFunctions.WriteToJson("../../../customers.json", Program.jsonData);
                     break;
-                }
+                case 1:
+                    while (true)
+                    {
+                        Console.WriteLine("Voer het nieuwe wachtwoord in");
+                        string newPassword = Console.ReadLine()!;
+                        Console.WriteLine("Bevestig het wachtwoord");
+
+                        if (newPassword == Console.ReadLine())
+                        {
+                            Program.jsonData[index].Wachtwoord = newPassword;
+                            JsonFunctions.WriteToJson("../../../customers.json", Program.jsonData);
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wachtwoorden komen niet overeen!");
+                        }
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Bent u student? (ja/nee)");
+
+                    while (true)
+                    {
+                        string studentInput = Console.ReadLine()!;
+
+                        if (studentInput == "ja")
+                        {
+                            Program.jsonData[index].IsStudent = true;
+                            break;
+                        }
+                        else if (studentInput == "nee")
+                        {
+                            Program.jsonData[index].IsStudent = false;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ongeldige invoer!");
+                        }
+                    }
+                    break;
+                
             }
+            JsonFunctions.WriteToJson("../../../customers.json", Program.jsonData);
+            System.Console.WriteLine("Uw gegevens zijn geupdated!. Druk op een willekeurige knop om terug te keren");
+            Console.ReadKey(true);
+            return;
         }
     }
 }
